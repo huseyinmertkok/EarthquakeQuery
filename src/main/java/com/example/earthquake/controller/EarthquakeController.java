334c7ac438091eac3a2e0a32fdb41ce07665b510
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-09-27 16:23:08 +0300

    Implement All the Information Needed to the Table
    New rows:
    • Index
    • Country
    • Date & Time

--- src/main/java/com/example/earthquake/controller/EarthquakeController.java
+++ src/main/java/com/example/earthquake/controller/EarthquakeController.java
@@ -1,9 +1,6 @@
 package com.example.earthquake.controller;
 
 import com.example.earthquake.model.Earthquake;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import netscape.javascript.JSObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,43 +9,58 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("/earthquake")
 public class EarthquakeController {
     private List<Earthquake> earthquakeList;
-    private final String API_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02";
-    private void loadDataToList() throws IOException, InterruptedException, JSONException {
-        earthquakeList = new ArrayList<>();
+    private final String API_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&";
+    @RequestMapping("/list")
+    private String listEarthquakes(Model model) throws IOException, InterruptedException, JSONException {
+        earthquakeList = loadDataToList(360, "Turkey");
+        model.addAttribute("earthquakes", earthquakeList);
+
+        return "earthquake-list";
+    }
+
+    private List<Earthquake> loadDataToList(int pastDaysCount, String country) throws IOException, InterruptedException, JSONException {
+        String endDate = LocalDate.now().toString();
+        String startDate = LocalDate.now().minusDays(pastDaysCount).toString();
+        List <Earthquake> tempList = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .header("Content-Type", "application/json")
-                .uri(URI.create(API_URL))
+                .uri(URI.create(API_URL + "starttime="+startDate+"&endtime="+endDate+"&minlatitude=36&maxlatitude=42&minlongitude=26&maxlongitude=45"))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         JSONObject jsonObject = new JSONObject(response.body());
         JSONArray jsonArray = jsonObject.getJSONArray("features");
-
-        for(int i=0; i<jsonArray.length(); i++){
-            earthquakeList.add(new Earthquake(
-                    jsonArray.getJSONObject(i).getJSONObject("properties").getString("place"),
-                    jsonArray.getJSONObject(i).getJSONObject("properties").getDouble("mag")
-            ));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        int id = 1;
+        if(jsonArray.length() > 0){
+            for(int i=0; i<jsonArray.length(); i++){
+                if(jsonArray.getJSONObject(i).getJSONObject("properties").getString("place").contains(country)){
+                    tempList.add(new Earthquake(
+                            id,
+                            country,
+                            jsonArray.getJSONObject(i).getJSONObject("properties").getString("place"),
+                            jsonArray.getJSONObject(i).getJSONObject("properties").getDouble("mag"),
+                            simpleDateFormat.format(new Date(jsonArray.getJSONObject(i).getJSONObject("properties").getLong("time")))
+                    ));
+                    id++;
+                }
+            }
         }
-    }
-
-    @RequestMapping("/list")
-    private String listEarthquakes(Model model) throws IOException, InterruptedException, JSONException {
-        loadDataToList();
-        model.addAttribute("earthquakes", earthquakeList);
-        return "earthquake-list";
+        return tempList;
     }
 }
--- src/main/java/com/example/earthquake/model/Earthquake.java
+++ src/main/java/com/example/earthquake/model/Earthquake.java
@@ -3,14 +3,22 @@ package com.example.earthquake.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class Earthquake {
+    private int id;
+    private String country;
     private String place;
     private Double mag;
+    private String time;
 
-    public Earthquake(String place, Double mag){
+    public Earthquake(int id, String country, String place, Double mag, String time){
+        this.id = id;
+        this.country = country;
         this.place = place;
         this.mag = mag;
+        this.time = time;
     }
 }
