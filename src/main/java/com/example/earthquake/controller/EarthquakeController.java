4f110244aba5dcd87357b2776a94627f99d9f213
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-09-28 12:03:11 +0300

    Removed unused imports

--- src/main/java/com/example/earthquake/controller/EarthquakeController.java
+++ src/main/java/com/example/earthquake/controller/EarthquakeController.java
@@ -1,27 +1,11 @@
 package com.example.earthquake.controller;
-
-import com.example.earthquake.model.Earthquake;
 import com.example.earthquake.service.EarthquakeService;
-import com.example.earthquake.utility.LatLng;
-import com.example.earthquake.utility.Util;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Controller
 @RequestMapping("/earthquake")
--- src/main/java/com/example/earthquake/model/Earthquake.java
+++ src/main/java/com/example/earthquake/model/Earthquake.java
@@ -3,8 +3,6 @@ package com.example.earthquake.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class Earthquake {
